bf7a58605ab42ca20f1f9c9d06d3bca561680b5e
David Valentine <david.valentine@gmail.com>
2022-08-02 16:31:42 -0700

    working to get a working push

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,7 +7,7 @@ module.exports = {
     ],
     images: {
         loader: "default",
-        domains: ['dummyimage.com','source.unsplash.com',"localhost","strapi-ynme.onrender.com",],
+        domains: ['dummyimage.com','source.unsplash.com',"localhost","strapi-ynme.onrender.com","strapi-jc84.onrender.com"],
     },
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
