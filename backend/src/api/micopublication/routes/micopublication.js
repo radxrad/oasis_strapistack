f68fe6fad06e170ac3ccc256ca469dd73d504e6c
David Valentine <david.valentine@gmail.com>
2022-08-09 15:28:01 -0700

    sync

--- backend/src/api/micopublication/routes/micopublication.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * micopublication router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::micopublication.micopublication');
